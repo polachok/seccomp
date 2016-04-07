3e20878c978687ae7daa592354ad00488e033648
Alexander Polakov <plhk@sdf.org>
2016-04-07 19:15:34 +0300

    Add errors, examples and docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,240 @@
+//! This crate is based on [seccomp_sys](https://crates.io/crates/seccomp-sys) and provides
+//! a higher level wrapper for [libseccomp](https://github.com/seccomp/libseccomp).
+//!
+//!
+//! Example usage:
+//!
+//! ```rust,no_run
+//!extern crate seccomp;
+//!extern crate libc;
+//!
+//!use seccomp::*;
+//!
+//!fn main() {
+//!		let mut ctx = Context::default(Action::Allow).unwrap();
+//!		let rule = Rule::new(105 /* setuid on x86_64 */,
+//!			Compare::arg(0)
+//! 			    .with(1000)
+//! 				.using(Op::Eq)
+//! 				.build().unwrap(),
+//!			Action::Errno(libc::EPERM) /* return EPERM */
+//!		);
+//!		ctx.add_rule(rule).unwrap();
+//!		ctx.load().unwrap();
+//!		let ret = unsafe { libc::setuid(1000) };
+//!		println!("ret = {}, uid = {}", ret, unsafe { libc::getuid() });
+//!}
+//!# pook perd
+//!
+
+
 extern crate seccomp_sys;
 extern crate libc;
 
 use seccomp_sys::*;
-pub use seccomp_sys::scmp_compare::*;
+use std::error::Error;
+use std::fmt;
+use std::convert::Into;
+use seccomp_sys::scmp_compare::*;
 
-#[derive(Debug)]
-pub enum Action {
-	Allow,
-	Kill,
-	Trap
+pub type Cmp = scmp_arg_cmp;
+
+/// Comparison operators
+#[derive(Debug,Clone,Copy)]
+pub enum Op {
+	/// not equal
+	Ne,
+	/// less than
+	Lt,
+	/// less than or equal
+	Le,
+	/// equal
+	Eq,
+	/// greater than or equal
+	Ge,
+	/// greater than
+	Gt,
+	/// masked equality
+	MaskedEq,
 }
 
-impl Action {
-	fn to_c(&self) -> libc::uint32_t {
-		match *self {
-			Action::Allow => SCMP_ACT_ALLOW,
-			Action::Kill => SCMP_ACT_KILL,
-			Action::Trap => SCMP_ACT_TRAP,
+impl Into<scmp_compare> for Op {
+	fn into(self) -> scmp_compare {
+		match self {
+			Op::Ne => SCMP_CMP_NE,
+			Op::Lt => SCMP_CMP_LT,
+			Op::Le => SCMP_CMP_LE,
+			Op::Eq => SCMP_CMP_EQ,
+			Op::Ge => SCMP_CMP_GE,
+			Op::Gt => SCMP_CMP_GT,
+			Op::MaskedEq => SCMP_CMP_MASKED_EQ,
 		}
 	}
 }
 
+/// Seccomp actions
+#[derive(Debug,Clone,Copy)]
+pub enum Action {
+	/// Allow the syscall to be executed
+	Allow,
+	/// Kill the process
+	Kill,
+	/// Throw a SIGSYS signal
+	Trap,
+	/// Return the specified error code
+	Errno(i32),
+	/// Notify a tracing process with the specified value
+	Trace(u32),
+}
+
+impl Into<libc::uint32_t> for Action {
+	fn into(self) -> libc::uint32_t {
+		match self {
+			Action::Allow => SCMP_ACT_ALLOW,
+			Action::Kill => SCMP_ACT_KILL,
+			Action::Trap => SCMP_ACT_TRAP,
+			Action::Errno(x) => SCMP_ACT_ERRNO(x as u32),
+			Action::Trace(x) => SCMP_ACT_TRACE(x),
+		}
+	}
+}
+
+/// Comparison definition builder
 pub struct Compare {
 	arg: libc::c_uint,
-	op: Option<scmp_compare>,
+	op: Option<Op>,
 	datum_a: Option<scmp_datum_t>,
 	datum_b: Option<scmp_datum_t>,
 }
 
 impl Compare {
+	/// argument number, starting at 0
 	pub fn arg(arg_num: u32) -> Self {
 		Compare { arg: arg_num as libc::c_uint, op: None, datum_a: None, datum_b: None }
 	}
 
-	pub fn using(mut self, op: scmp_compare) -> Self {
+	/// Comparison operator
+	pub fn using(mut self, op: Op) -> Self {
 		self.op = Some(op);
 		self
 	}
 
+	/// Datum to compare with
 	pub fn with(mut self, datum: u64) -> Self {
 		self.datum_a = Some(datum);
 		self
 	}
 
-
+	/// Second datum
 	pub fn and(mut self, datum: u64) -> Self {
 		self.datum_b = Some(datum);
 		self
 	}
 
-	pub fn build(self) -> Option<scmp_arg_cmp> {
+	/// build comparison definition
+	pub fn build(self) -> Option<Cmp> {
 		if self.op.is_some() && self.datum_a.is_some() {
-			Some(scmp_arg_cmp { arg: self.arg, op: self.op.unwrap(), datum_a: self.datum_a.unwrap(), datum_b: self.datum_b.unwrap_or(0) })
+			Some(Cmp {
+				 arg: self.arg,
+				 op: self.op.unwrap().into(),
+				 datum_a: self.datum_a.unwrap(),
+				 datum_b: self.datum_b.unwrap_or(0)
+			 })
 		} else {
 			None
 		}
 	}
 }
 
+/// Seccomp rule
+#[derive(Debug)]
 pub struct Rule {
 	action: Action,
 	syscall_nr: usize,
-	comparators: Vec<scmp_arg_cmp>,
+	comparators: Vec<Cmp>,
 }
 
 impl Rule {
-	pub fn new(syscall_nr: usize, cmp: scmp_arg_cmp, action: Action) -> Rule {
+	/// Create new rule for `syscall_nr` using comparison `cmp`.
+	pub fn new(syscall_nr: usize, cmp: Cmp, action: Action) -> Rule {
 		Rule {
 			action: action,
 			syscall_nr: syscall_nr,
 			comparators: vec![cmp]
 		}
 	}
+
+	/// Adds comparison. Multiple comparisons will be
+	/// ANDed together.
+	pub fn add_comparison(&mut self, cmp: Cmp) {
+		self.comparators.push(cmp);
+	}
 }
 
+/// Error type
+#[derive(Debug)]
+pub struct SeccompError {
+	msg: String
+}
+
+impl SeccompError {
+	fn new<T: Into<String>>(msg: T) -> Self {
+		SeccompError {
+			msg: msg.into()
+		}
+	}
+}
+
+impl fmt::Display for SeccompError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+			write!(f, "SeccompError: {}", self.msg)
+	}
+}
+
+impl Error for SeccompError {
+	fn description(&self) -> &str {
+		&self.msg
+	}
+}
+
+/// Seccomp context
 #[derive(Debug)]
 pub struct Context {
 	int: *mut scmp_filter_ctx,
 }
 
 impl Context {
-	pub fn default(def_action: Action) -> Context {
-		Context { int: unsafe { seccomp_init(def_action.to_c()) } }
+	/// Creates new context with default action
+	pub fn default(def_action: Action) -> Result<Context,SeccompError> {
+		let filter_ctx = unsafe { seccomp_init(def_action.into()) };
+		if filter_ctx.is_null() {
+			return Err(SeccompError::new("initialization failed"));
+		}
+		Ok(Context { int: filter_ctx })
 	}
 
-	pub fn add_rule(&mut self, rule: Rule) {
-		unsafe { seccomp_rule_add(self.int, rule.action.to_c(), rule.syscall_nr as i32, rule.comparators.len() as u32, rule.comparators); }
+	/// Adds rule to the context
+	pub fn add_rule(&mut self, rule: Rule) -> Result<(),SeccompError> {
+		let res = unsafe {
+			 seccomp_rule_add_array(self.int, rule.action.into(),
+				 rule.syscall_nr as i32, rule.comparators.len() as u32,
+				 rule.comparators.as_slice().as_ptr())
+		};
+		if res != 0 {
+			Err(SeccompError::new(format!("failed to add rule {:?}", rule)))
+		} else {
+			Ok(())
+		}
 	}
 
-	pub fn load(&self) {
-		unsafe { seccomp_load(self.int) };
+	/// Loads the filter into the kernel. Rules will be applied when this function returns.
+	pub fn load(&self) -> Result<(),SeccompError> {
+		let res = unsafe { seccomp_load(self.int) };
+		if res != 0 {
+			Err(SeccompError::new("failed to load filter into the kernel"))
+		} else {
+			Ok(())
+		}
 	}
 }
 
@@ -101,9 +246,13 @@ impl Drop for Context {
 
 #[test]
 fn it_works() {
-	use std::fs::File;
-	let mut ctx = Context::default(Action::Allow);
-	ctx.add_rule(Rule::new(105, Compare::arg(0).using(SCMP_CMP_EQ).with(1000).build().unwrap(), Action::Kill));
-	ctx.load();
-	File::open("/etc/passwd");
+	fn test() -> Result<(),Box<Error>> {
+		let mut ctx = try!(Context::default(Action::Allow));
+		try!(ctx.add_rule(Rule::new(105, Compare::arg(0).using(Op::Eq).with(1000).build().unwrap(), Action::Errno(libc::EPERM))));
+		try!(ctx.load());
+		let ret = unsafe { libc::setuid(1000) };
+		println!("ret = {}, uid = {}", ret, unsafe { libc::getuid() });
+		Ok(())
+	}
+	test().unwrap();
 }
